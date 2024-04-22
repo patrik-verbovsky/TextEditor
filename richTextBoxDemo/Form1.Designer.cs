cca7f5257d65713a9f6f0a41c44e27b9e9b96fc4
Patrik Verbovský <118107098+patrik-verbovsky@users.noreply.github.com>
2024-04-22 18:33:40 +0200

    soubory

---
+++ richTextBoxDemo/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace richTextBoxDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            button2 = new Button();
+            button1 = new Button();
+            button3 = new Button();
+            menuStrip1 = new MenuStrip();
+            souborToolStripMenuItem = new ToolStripMenuItem();
+            uložitToolStripMenuItem = new ToolStripMenuItem();
+            oToolStripMenuItem = new ToolStripMenuItem();
+            nápovědaToolStripMenuItem = new ToolStripMenuItem();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            fontDialog1 = new FontDialog();
+            button4 = new Button();
+            checkBox1 = new CheckBox();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 57);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(1065, 613);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.ImeMode = ImeMode.Off;
+            comboBox1.Items.AddRange(new object[] { "Zelená", "Žlutá", "Modrá", "Černá", "Růžová", "Červená" });
+            comboBox1.Location = new Point(111, 28);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.Text = "Barva";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Zelená", "Žlutá", "Modrá", "Růžová", "Červená", "Žádná" });
+            comboBox2.Location = new Point(238, 28);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 2;
+            comboBox2.Text = "Barva pozadí";
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "8", "12", "16", "20", "24", "32", "48", "64", "72" });
+            comboBox3.Location = new Point(365, 28);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(63, 23);
+            comboBox3.TabIndex = 3;
+            comboBox3.Text = "Velikost";
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button2.Location = new Point(12, 27);
+            button2.Name = "button2";
+            button2.Size = new Size(27, 23);
+            button2.TabIndex = 5;
+            button2.Text = "B";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 9F, FontStyle.Italic, GraphicsUnit.Point, 238);
+            button1.Location = new Point(45, 27);
+            button1.Name = "button1";
+            button1.RightToLeft = RightToLeft.No;
+            button1.Size = new Size(27, 23);
+            button1.TabIndex = 6;
+            button1.Text = "I";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Underline, GraphicsUnit.Point, 238);
+            button3.Location = new Point(78, 27);
+            button3.Name = "button3";
+            button3.Size = new Size(27, 23);
+            button3.TabIndex = 7;
+            button3.Text = "U";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { souborToolStripMenuItem, nápovědaToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1089, 24);
+            menuStrip1.TabIndex = 8;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // souborToolStripMenuItem
+            // 
+            souborToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { uložitToolStripMenuItem, oToolStripMenuItem });
+            souborToolStripMenuItem.Name = "souborToolStripMenuItem";
+            souborToolStripMenuItem.Size = new Size(57, 20);
+            souborToolStripMenuItem.Text = "Soubor";
+            // 
+            // uložitToolStripMenuItem
+            // 
+            uložitToolStripMenuItem.Name = "uložitToolStripMenuItem";
+            uložitToolStripMenuItem.Size = new Size(116, 22);
+            uložitToolStripMenuItem.Text = "Uložit...";
+            uložitToolStripMenuItem.Click += uložitToolStripMenuItem_Click;
+            // 
+            // oToolStripMenuItem
+            // 
+            oToolStripMenuItem.Name = "oToolStripMenuItem";
+            oToolStripMenuItem.Size = new Size(116, 22);
+            oToolStripMenuItem.Text = "Otevřít..";
+            oToolStripMenuItem.Click += oToolStripMenuItem_Click;
+            // 
+            // nápovědaToolStripMenuItem
+            // 
+            nápovědaToolStripMenuItem.Name = "nápovědaToolStripMenuItem";
+            nápovědaToolStripMenuItem.Size = new Size(73, 20);
+            nápovědaToolStripMenuItem.Text = "Nápověda";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1002, 28);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 9;
+            button4.Text = "Ostatní...";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(952, 32);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(44, 19);
+            checkBox1.TabIndex = 10;
+            checkBox1.Text = "List";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripStatusLabel2 });
+            statusStrip1.Location = new Point(0, 673);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1089, 22);
+            statusStrip1.TabIndex = 11;
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(0, 17);
+            toolStripStatusLabel2.Click += toolStripStatusLabel2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1089, 695);
+            Controls.Add(statusStrip1);
+            Controls.Add(checkBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(richTextBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Textový editor";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private Button button2;
+        private Button button1;
+        private Button button3;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem souborToolStripMenuItem;
+        private ToolStripMenuItem uložitToolStripMenuItem;
+        private ToolStripMenuItem nápovědaToolStripMenuItem;
+        private ToolStripMenuItem oToolStripMenuItem;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private FontDialog fontDialog1;
+        private Button button4;
+        private CheckBox checkBox1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
---
+++ richTextBoxDemo/Form1.cs
@@ -0,0 +1,219 @@
+namespace richTextBoxDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripStatusLabel2.Text = "Program naèten!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "Modrá")
+            {
+                richTextBox1.SelectionColor = Color.Blue;
+                toolStripStatusLabel2.Text = "Vybráná: modrá";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Zelená")
+            {
+                richTextBox1.SelectionColor = Color.Green;
+                toolStripStatusLabel2.Text = "Vybráná: zelená";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Žlutá")
+            {
+                richTextBox1.SelectionColor = Color.Yellow;
+                toolStripStatusLabel2.Text = "Vybráná: žlutá";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Èervená")
+            {
+                richTextBox1.SelectionColor = Color.Red;
+                toolStripStatusLabel2.Text = "Vybráná: èervená";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Èerná")
+            {
+                richTextBox1.SelectionColor = Color.Black;
+                toolStripStatusLabel2.Text = "Vybráná: èerná";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Rùžová")
+            {
+                richTextBox1.SelectionColor = Color.Pink;
+                toolStripStatusLabel2.Text = "Vybráná: rùžová";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Modrá")
+            {
+                richTextBox1.SelectionColor = Color.Blue;
+                toolStripStatusLabel2.Text = "Vybráná: modrá";
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem.ToString() == "Modrá")
+            {
+                richTextBox1.SelectionBackColor = Color.Blue;
+                toolStripStatusLabel2.Text = "Vybráná: modrá";
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Zelená")
+            {
+                richTextBox1.SelectionBackColor = Color.Green;
+                toolStripStatusLabel2.Text = "Vybráná: zelená";
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Žlutá")
+            {
+                richTextBox1.SelectionBackColor = Color.Yellow;
+                toolStripStatusLabel2.Text = "Vybráná: žlutá";
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Èervená")
+            {
+                richTextBox1.SelectionBackColor = Color.Red;
+                toolStripStatusLabel2.Text = "Vybráná: èervená";
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Rùžová")
+            {
+                richTextBox1.SelectionBackColor = Color.Pink;
+                toolStripStatusLabel2.Text = "Vybráná: rùžová";
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Modrá")
+            {
+                richTextBox1.SelectionBackColor = Color.Blue;
+                toolStripStatusLabel2.Text = "Vybráná: modrá";
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Žádná")
+            {
+                richTextBox1.SelectionBackColor = Color.White;
+                toolStripStatusLabel2.Text = "Vybráná: žádná";
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.SelectedItem.ToString() == "8")
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.Font.FontFamily, 8, richTextBox1.Font.Style);
+                toolStripStatusLabel2.Text = "Velikost 8";
+            }
+            else if (comboBox3.SelectedItem.ToString() == "12")
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.Font.FontFamily, 12, richTextBox1.Font.Style);
+                toolStripStatusLabel2.Text = "Velikost 12";
+            }
+            else if (comboBox3.SelectedItem.ToString() == "16")
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.Font.FontFamily, 16, richTextBox1.Font.Style);
+                toolStripStatusLabel2.Text = "Velikost 16";
+            }
+            else if (comboBox3.SelectedItem.ToString() == "20")
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.Font.FontFamily, 20, richTextBox1.Font.Style);
+                toolStripStatusLabel2.Text = "Velikost 20";
+            }
+            else if (comboBox3.SelectedItem.ToString() == "24")
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.Font.FontFamily, 24, richTextBox1.Font.Style);
+                toolStripStatusLabel2.Text = "Velikost 24";
+            }
+            else if (comboBox3.SelectedItem.ToString() == "32")
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.Font.FontFamily, 32, richTextBox1.Font.Style);
+                toolStripStatusLabel2.Text = "Velikost 32";
+            }
+            else if (comboBox3.SelectedItem.ToString() == "48")
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.Font.FontFamily, 48, richTextBox1.Font.Style);
+                toolStripStatusLabel2.Text = "Velikost 48";
+            }
+            else if (comboBox3.SelectedItem.ToString() == "64")
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.Font.FontFamily, 64, richTextBox1.Font.Style);
+                toolStripStatusLabel2.Text = "Velikost 64";
+            }
+            else if (comboBox3.SelectedItem.ToString() == "72")
+            {
+                richTextBox1.SelectionFont = new Font(richTextBox1.Font.FontFamily, 72, richTextBox1.Font.Style);
+                toolStripStatusLabel2.Text = "Velikost 72";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Font vyberfont = richTextBox1.SelectionFont;
+            richTextBox1.SelectionFont = new Font(vyberfont, FontStyle.Bold);
+            toolStripStatusLabel2.Text = "Vybráno tuèné písmo";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Font vyberfont = richTextBox1.SelectionFont;
+            richTextBox1.SelectionFont = new Font(vyberfont, FontStyle.Italic);
+            toolStripStatusLabel2.Text = "Vybrána kurzíva";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Font vyberfont = richTextBox1.SelectionFont;
+            richTextBox1.SelectionFont = new Font(vyberfont, FontStyle.Underline);
+            toolStripStatusLabel2.Text = "Vybráno podtržené písmo";
+        }
+
+        private void uložitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Vybírání lokace k uložení souboru...";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                toolStripStatusLabel2.Text = "Ukládám soubor...";
+                richTextBox1.SaveFile(saveFileDialog1.FileName);
+                toolStripStatusLabel2.Text = "Soubor uložen!";
+            }
+        }
+
+        private void oToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Otvírání souboru...";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                toolStripStatusLabel2.Text = "Naèítám soubor...";
+                richTextBox1.LoadFile(openFileDialog1.FileName);
+                toolStripStatusLabel2.Text = "Soubor naèten!";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Spouštím jiné možnosti...";
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+                toolStripStatusLabel2.Text = "Možnosti uloženy!";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                richTextBox1.SelectionBullet = true;
+                toolStripStatusLabel2.Text = "Zaškrtnut list!";
+            }
+            else
+            {
+                richTextBox1.SelectionBullet = false;
+                toolStripStatusLabel2.Text = "Odškrtnut list!";
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
